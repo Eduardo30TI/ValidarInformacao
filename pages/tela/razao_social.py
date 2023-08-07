ec4580793e8eb19fbc141ead722df2c1d4af238b
TI <121513063+Eduardo30TI@users.noreply.github.com>
2023-08-07 13:01:45 -0300

    Add files via upload

--- pages/tela/razao_social.py
+++ pages/tela/razao_social.py
@@ -46,20 +46,29 @@ class Consultar:
                     perc=float(round(cont/len(lista),4))
                     bar.progress(perc)
                     texto.write(f'{cont} de {len(lista)}')                    
+
+                    try:
            
-                    cnpj=CNPJ(c)
-                    json=cnpj.GetDados()
+                        cnpj=CNPJ(c)
+                        json=cnpj.GetDados()
                     
-                    temp_dict={'razao_social':'Razão Social','nome_fantasia':'Nome Fantasia'}
+                        temp_dict={'razao_social':'Razão Social','nome_fantasia':'Nome Fantasia'}
 
-                    for k,v in temp_dict.items():
+                        for k,v in temp_dict.items():
 
-                        nome=json[k]
+                            nome=json[k]
  
-                        df.loc[df['CNPJ']==c,v]=nome
+                            df.loc[df['CNPJ']==str(c),v]=nome
+
+                            pass
 
                         pass
 
+                    except Exception as erro:
+
+                        print(erro)
+
+                        continue
 
                     #break
 
